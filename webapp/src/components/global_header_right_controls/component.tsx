904cec66db6edbae4edf6dd0844ac3d2206b8426
Claudio Costa <cstcld91@gmail.com>
2021-09-17 16:24:14 +0200

    Sync icons with design

--- webapp/src/components/global_header_right_controls/component.tsx
+++ webapp/src/components/global_header_right_controls/component.tsx
@@ -4,10 +4,11 @@ import {FormattedMessage} from 'react-intl';
 
 import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMicrophoneAlt, faMicrophoneAltSlash, faPhoneSlash} from '@fortawesome/free-solid-svg-icons';
-
 import Avatar, {TAvatarSizeToken} from '../avatar';
+
+import MutedIcon from 'components/icons/muted_icon';
+import UnmutedIcon from 'components/icons/unmuted_icon';
+import LeaveCallIcon from 'components/icons/leave_call_icon';
 
 export default class GlobalHeaderRightControls extends React.PureComponent {
     static propTypes = {
@@ -77,7 +78,7 @@ export default class GlobalHeaderRightControls extends React.PureComponent {
                         className='user_call_status'
                         style={{display: isMuted ? 'block' : 'none', position: 'absolute', top: 'auto', bottom: '-4px', right: '-4px', fontSize: '12px', color: 'red'}}
                     >
-                        <FontAwesomeIcon icon={faMicrophoneAltSlash}/>
+                      <MutedIcon fill="red"/>
                     </div>
                 </div>
             );
@@ -89,8 +90,8 @@ export default class GlobalHeaderRightControls extends React.PureComponent {
             return null;
         }
 
-        const muteIcon = this.state.isMuted ? faMicrophoneAltSlash : faMicrophoneAlt;
-        const muteStyle = this.state.isMuted ? style.unmuteButton : style.muteButton;
+        const MuteIcon = this.state.isMuted ? MutedIcon : UnmutedIcon;
+        const muteIconStyle = this.state.isMuted ? style.MutedIcon : style.UnmutedIcon;
         const muteTooltipText = this.state.isMuted ? 'Unmute' : 'Mute';
 
         return (
@@ -115,10 +116,10 @@ export default class GlobalHeaderRightControls extends React.PureComponent {
                             <button
                                 id='voice-mute-unmute'
                                 className='cursor--pointer style--none'
-                                style={muteStyle}
+                                style={style.muteButton}
                                 onClick={this.onMuteToggle}
                             >
-                                <FontAwesomeIcon icon={muteIcon}/>
+                                <MuteIcon fill='rgba(255, 255, 255, 0.8)'/>
                             </button>
                         </OverlayTrigger>
 
@@ -138,22 +139,10 @@ export default class GlobalHeaderRightControls extends React.PureComponent {
                                 style={style.disconnectButton}
                                 onClick={this.onDisconnectClick}
                             >
-                                {/* <FontAwesomeIcon icon={faPhoneSlash}/> */}
-
-                                <svg
-                                    width='16'
-                                    height='6'
-                                    viewBox='0 0 16 6'
-                                    fill='none'
-                                    xmlns='http://www.w3.org/2000/svg'
+                                <LeaveCallIcon
                                     style={{width: '20px', height: '20px'}}
-                                >
-                                    <path
-                                        d='M8 1.8C7.04375 1.8 6.125 1.94063 5.24375 2.22188V4.07813C5.24375 4.34063 5.13125 4.52813 4.90625 4.64063C4.34375 4.92188 3.80938 5.2875 3.30313 5.7375C3.19063 5.85 3.05 5.90625 2.88125 5.90625C2.7125 5.90625 2.57188 5.85 2.45938 5.7375L0.96875 4.24688C0.85625 4.13438 0.8 3.99375 0.8 3.825C0.8 3.65625 0.85625 3.51563 0.96875 3.40313C1.90625 2.52188 2.96563 1.8375 4.14688 1.35C5.38438 0.84375 6.66875 0.590625 8 0.590625C9.33125 0.590625 10.6156 0.84375 11.8531 1.35C13.0344 1.8375 14.0937 2.52188 15.0312 3.40313C15.1437 3.51563 15.2 3.65625 15.2 3.825C15.2 3.99375 15.1437 4.13438 15.0312 4.24688L13.5406 5.7375C13.4281 5.85 13.2875 5.90625 13.1187 5.90625C12.95 5.90625 12.8094 5.85 12.6969 5.7375C12.2094 5.2875 11.675 4.92188 11.0937 4.64063C10.8687 4.52813 10.7562 4.34063 10.7562 4.07813V2.22188C9.875 1.94063 8.95625 1.8 8 1.8Z'
-                                        fill='white'
-                                    />
-                                </svg>
-
+                                    fill='white'
+                                />
                             </button>
                         </OverlayTrigger>
 
@@ -173,18 +162,14 @@ const style = {
         display: 'flex',
     },
     muteButton: {
-        fontSize: '15px',
-        margin: '0 8px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
         width: '24px',
-        heigth: '24px',
-        color: 'color: rgba(255, 255, 255, 0.8)',
     },
-    unmuteButton: {
-        fontSize: '15px',
-        margin: '0 8px',
-        width: '24px',
-        heigth: '24px',
-        color: 'color: rgba(255, 255, 255, 0.8)',
+    mutedIcon: {
+    },
+    unmutedIcon: {
     },
     disconnectButton: {
         display: 'flex',
