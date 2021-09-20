c1f4544afcbe13039e42122690d2ef852d5ec725
Claudio Costa <cstcld91@gmail.com>
2021-09-20 12:29:40 +0200

    Extract connected  profiles into own component

---
+++ webapp/src/components/connected_profiles.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
+
+import Avatar, {TAvatarSizeToken} from './avatar';
+
+export default class ConnectedProfiles extends React.PureComponent {
+    static propTypes = {
+        pictures: PropTypes.array,
+        profiles: PropTypes.array,
+        size: PropTypes.string,
+        maxShowedProfiles: PropTypes.number,
+    }
+
+    render() {
+        const maxShowedProfiles = this.props.maxShowedProfiles || 2;
+        const diff = this.props.profiles.length - maxShowedProfiles;
+        const profiles = diff > 0 ? this.props.profiles.slice(0, maxShowedProfiles) : this.props.profiles;
+
+        let off = 0;
+
+        const els = profiles.map((profile, idx) => {
+            off += 8;
+            return (
+                <OverlayTrigger
+                    placement='bottom'
+                    key={'call_thread_profile_' + profile.id}
+                    overlay={
+                        <Tooltip id='tooltip-username'>
+                            { profile.username }
+                        </Tooltip>
+                    }
+                >
+                    <Avatar
+                        size={this.props.size}
+                        url={this.props.pictures[idx]}
+                        style={{position: 'relative', left: `-${off}px`}}
+                    />
+                </OverlayTrigger>
+            );
+        });
+
+        if (diff > 0) {
+            off += 8;
+            els.push(
+                <Avatar
+                    size={this.props.size}
+                    text={`+${diff}`}
+                    style={{position: 'relative', left: `-${off}px`, background: '#efeff0'}}
+                    key='call_thread_more_profiles'
+                />,
+            );
+        }
+
+        return els;
+    }
+}
