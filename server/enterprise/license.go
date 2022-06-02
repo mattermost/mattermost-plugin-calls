a08a257cd3770d4dffbfe9eee671ecfe14d616f9
streamer45 <cstcld91@gmail.com>
2022-06-02 17:03:30 +0200

    Bump license requirement

--- server/enterprise/license.go
+++ server/enterprise/license.go
@@ -17,17 +17,17 @@ func NewLicenseChecker(pluginAPIClient *pluginapi.Client) *LicenseChecker {
 	}
 }
 
-// isAtLeastE10Licensed returns true when the server either has at least an E10 license or is configured for development.
-func (e *LicenseChecker) isAtLeastE10Licensed() bool {
+// isAtLeastE20Licensed returns true when the server either has at least an E20 license or is configured for development.
+func (e *LicenseChecker) isAtLeastE20Licensed() bool {
 	config := e.pluginAPIClient.Configuration.GetConfig()
 	license := e.pluginAPIClient.System.GetLicense()
 
-	return pluginapi.IsE10LicensedOrDevelopment(config, license)
+	return pluginapi.IsE20LicensedOrDevelopment(config, license)
 }
 
 // RTCDAllowed returns true if the license allows use of an external rtcd service.
 func (e *LicenseChecker) RTCDAllowed() bool {
 	license := e.pluginAPIClient.System.GetLicense()
 
-	return e.isAtLeastE10Licensed() || pluginapi.IsCloud(license)
+	return e.isAtLeastE20Licensed() || pluginapi.IsCloud(license)
 }
