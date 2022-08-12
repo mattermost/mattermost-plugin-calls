693aaae58ff7b563a5c7d8bccc39228b3753ccef
AGMETEOR <guwats10@gmail.com>
2022-08-12 17:49:18 +0300

    fix name

--- webapp/src/actions.ts
+++ webapp/src/actions.ts
@@ -134,7 +134,7 @@ export const displayCloudPricing = () => {
             return {};
         }
 
-        openPricingModal()({callerInfo: 'calls > '});
+        openPricingModal()({trackingLocation: 'calls > '});
         return {};
     };
 };
