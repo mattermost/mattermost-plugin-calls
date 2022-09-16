8a848ea22c16b79c6c4f1c1adc528a2f258f7a3b
streamer45 <cstcld91@gmail.com>
2022-09-16 11:10:59 +0200

    Some wording fixes

--- webapp/src/constants.ts
+++ webapp/src/constants.ts
@@ -9,20 +9,20 @@ export const CallAlertConfigs: {[key: string]: CallAlertConfig} = {
         icon: 'microphone',
         bannerText: 'Unable to find a valid audio input device. Try plugging in an audio input device.',
         tooltipText: 'No audio input devices',
-        tooltipSubtext: 'Try plugging in an audio input device',
+        tooltipSubtext: 'Try plugging in an audio input device.',
     },
     missingAudioInputPermissions: {
         type: CallAlertType.Error,
         icon: 'microphone',
         bannerText: 'Allow microphone access to Mattermost.',
         tooltipText: 'No audio input permissions',
-        tooltipSubtext: 'Allow microphone access to Mattermost',
+        tooltipSubtext: 'Allow microphone access to Mattermost.',
     },
     missingScreenPermissions: {
         type: CallAlertType.Error,
         icon: 'monitor',
         bannerText: 'Allow screen recording access to Mattermost in your system preferences.',
-        tooltipText: 'No screen-sharing permissions',
+        tooltipText: 'No screen sharing permissions',
         tooltipSubtext: 'Allow screen recording access to Mattermost.',
     },
 };
