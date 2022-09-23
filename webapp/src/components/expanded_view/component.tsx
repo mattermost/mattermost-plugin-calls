d93e62a9557f2381402ee7e50b3d7d9ad4818b3d
AGMETEOR <guwats10@gmail.com>
2022-09-23 12:22:59 +0300

    Close picker on click outside

--- webapp/src/components/expanded_view/component.tsx
+++ webapp/src/components/expanded_view/component.tsx
@@ -229,6 +229,7 @@ export default class ExpandedView extends React.PureComponent<Props, State> {
                     emojiVersion={EMOJI_VERSION}
                     skinTonePosition='search'
                     onEmojiSelect={this.handleUserPicksEmoji}
+                    onClickOutside={this.toggleEmojiPicker}
                 />
             </div>
         ) : null;
@@ -621,7 +622,10 @@ export default class ExpandedView extends React.PureComponent<Props, State> {
 
                                     <button
                                         className='button-center-controls'
-                                        onClick={this.toggleEmojiPicker}
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            this.toggleEmojiPicker();
+                                        }}
                                         style={{background: this.state.showEmojiPicker ? '#FFFFFF' : '', position: 'relative'}}
                                     >
                                         <SmileyIcon
