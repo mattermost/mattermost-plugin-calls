8f9d42663cbe66f874b57b9fc2400510ec0f7a24
Guillermo Vayá <guivaya@gmail.com>
2022-09-26 09:59:22 +0200

    Hands up (#208)
    * Use ReactDOM webpack external
    
    * basics working
    
    * chips and some fixes
    
    * Update webapp/src/components/reaction_stream/reaction_stream.tsx
    
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
    
    * Update webapp/src/components/reaction_stream/reaction_stream.tsx
    
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
    
    * add fixes
    
    * moved emojis to separate component
    
    * linting, changing the width
    
    * multiline chip
    
    * add suggestions
    
    * hands up - untested
    
    * Everybody hands up! this is not a robbery
    
    * improve hands up logic
    
    Co-authored-by: Michael Kochell <6913320+mickmister@users.noreply.github.com>
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>

--- webapp/src/components/reaction_stream/reaction_stream.tsx
+++ webapp/src/components/reaction_stream/reaction_stream.tsx
@@ -14,6 +14,7 @@ type Props = {
     reactions: ReactionWithUser[],
     currentUserID: string,
     profiles: {[key: string]: UserProfile;},
+    handsup: string[],
 };
 
 const ReactionStreamList = styled.div`
@@ -26,7 +27,11 @@ const ReactionStreamList = styled.div`
     mask: linear-gradient(#0000, #0003, #000f);
 `;
 
-const ReactionChip = styled.div`
+interface chipProps {
+    highlight?: boolean;
+}
+
+const ReactionChip = styled.div<chipProps>`
     display: flex;
     flex-direction: row;
     align-items: flex-end;
@@ -37,15 +42,19 @@ const ReactionChip = styled.div`
     border-radius: 12px;
     margin: 4px 0;
     width: fit-content;
+
+    ${(props) => props.highlight && `
+        background: #FFBC1F;
+        color: #090A0B;
+  `}
 `;
 
 // add a list of reactions, on top of that add the hands up as the top element
 export const ReactionStream = (props: Props) => {
     const reversed = [...props.reactions];
 
-    // add hands up into reversed
     reversed.reverse();
-    const elements = reversed.map((reaction) => {
+    const reactions = reversed.map((reaction) => {
         // emojis should be a separate component that is reused both here and in the extended view
         // getEmojiURL should be memoized as people tend to react similarly and this would speed up the process.
         const emoji = (<Emoji emoji={reaction.emoji}/>);
@@ -58,6 +67,43 @@ export const ReactionStream = (props: Props) => {
             </ReactionChip>
         );
     });
+
+    // add hands up
+    let elements = [];
+    const getName = (user_id: string) => {
+        return user_id === props.currentUserID ? 'You' : getUserDisplayName(props.profiles[user_id]);
+    };
+    let participants: string;
+    if (props.handsup?.length) {
+        switch (props.handsup?.length) {
+        case 1:
+            participants = `${getName(props.handsup[0])}`;
+            break;
+        case 2:
+            participants = `${getName(props.handsup[0])} & ${getName(props.handsup[1])}`;
+            break;
+        case 3:
+            participants = `${getName(props.handsup[0])}, ${getName(props.handsup[1])} & ${getName(props.handsup[2])}`;
+            break;
+        default:
+            participants = `${getName(props.handsup[0])}, ${getName(props.handsup[1])} & ${props.handsup?.length - 2} others`;
+            break;
+        }
+        const handsupElement = (<Emoji emoji={{name: 'hand', skin: '', unified: '270B'}}/>);
+
+        elements.push(
+            <ReactionChip
+                key={'hands'}
+                highlight={true}
+            >
+                <span>{handsupElement}</span>
+                &nbsp;
+                <span>{`${participants} raised a hand`}</span>
+            </ReactionChip>);
+    }
+
+    elements = [...elements, ...reactions];
+
     return (
         <ReactionStreamList>
             {elements}
