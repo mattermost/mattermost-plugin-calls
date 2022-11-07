2cf7071a6516e8c79762df914d5232254db340dc
Caleb Roseland <caleb@calebroseland.com>
2022-11-07 10:20:48 -0600

    e2e: supports chat

--- e2e/tests/popout.spec.ts
+++ e2e/tests/popout.spec.ts
@@ -40,5 +40,32 @@ test.describe('popout window', () => {
 
         await popOut.locator('.button-leave').click();
     });
+
+    test('supports chat', async ({page, context}) => {
+        const devPage = new PlaywrightDevPage(page);
+        await devPage.goto();
+        await devPage.startCall();
+
+        const [popOut, _] = await Promise.all([
+            context.waitForEvent('page'),
+            page.click('#calls-widget-expand-button'),
+        ]);
+        await expect(popOut.locator('#calls-expanded-view')).toBeVisible();
+
+        await popOut.click('#calls-popout-chat-button button');
+
+        await expect(popOut.locator('#sidebar-right [data-testid=call-thread]')).toBeVisible();
+
+        const replyTextbox = popOut.locator('#reply_textbox');
+        const msg = 'Hello World, first call thread reply';
+        await replyTextbox.type(msg);
+        await replyTextbox.press('Enter');
+        await expect(popOut.locator(`text=${msg}`)).toBeVisible();
+
+        await popOut.click('#calls-popout-chat-button button');
+        await expect(popOut.locator('#sidebar-right')).not.toBeVisible();
+
+        await popOut.locator('.button-leave').click();
+    });
 });
 
