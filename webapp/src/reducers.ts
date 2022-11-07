a69b29de857c56c3824d4b70e13c8785b7a150b5
Claudio Costa <cstcld91@gmail.com>
2022-11-07 08:45:37 +0100

    Fix state on parent window (#235)

--- webapp/src/reducers.ts
+++ webapp/src/reducers.ts
@@ -41,6 +41,7 @@ import {
     RECEIVED_CHANNEL_STATE,
     RECEIVED_CALLS_USER_PREFERENCES,
     RECEIVED_CLIENT_ERROR,
+    DESKTOP_WIDGET_CONNECTED,
 } from './action_types';
 
 interface channelState {
@@ -178,6 +179,8 @@ const connectedChannelID = (state: string | null = null, action: { type: string,
     switch (action.type) {
     case VOICE_CHANNEL_UNINIT:
         return null;
+    case DESKTOP_WIDGET_CONNECTED:
+        return action.data.channelID;
     case VOICE_CHANNEL_USER_CONNECTED: {
         const callsClient = window.callsClient || window.opener?.callsClient;
         if (action.data.currentUserID === action.data.userID && callsClient?.channelID === action.data.channelID) {
