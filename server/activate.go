0c2d0e3709c62586fb71bcada3d0d99b4f2a7bed
streamer45 <cstcld91@gmail.com>
2023-02-09 14:54:14 -0600

    Update calls-recorder

--- server/activate.go
+++ server/activate.go
@@ -104,6 +104,14 @@ func (p *Plugin) OnActivate() error {
 			return err
 		}
 
+		recorderVersion, ok := manifest.Props["calls_recorder_version"].(string)
+		if !ok {
+			err = fmt.Errorf("failed to get recorder version from manifest")
+			p.LogError(err.Error())
+			return err
+		}
+		recordingJobRunner = "mattermost/calls-recorder:" + recorderVersion
+
 		go func() {
 			p.LogDebug("updating job runner")
 
