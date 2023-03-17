71b33ed3ba06b1233809c7fcfb96daa4304ad530
streamer45 <cstcld91@gmail.com>
2023-03-17 14:52:07 -0600

    Use correct channelID when setting profile pictures in store

--- standalone/src/recording/index.tsx
+++ standalone/src/recording/index.tsx
@@ -119,7 +119,7 @@ async function wsHandlerRecording(store: Store, ev: WebSocketMessage<WebsocketEv
         store.dispatch({
             type: RECEIVED_CALL_PROFILE_IMAGES,
             data: {
-                channelID: ev.broadcast.channel_id,
+                channelID: ev.data.channelID,
                 profileImages: await fetchProfileImages(profiles),
             },
         });
