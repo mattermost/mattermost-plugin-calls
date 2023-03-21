b2d94b4a181f5f75a1086bb01ee2eaffbf69d54c
Claudio Costa <cstcld91@gmail.com>
2023-03-21 14:11:02 -0600

    [MM-51558] Stop recording job on bot disconnect or call end (#377)
    * Stop recording job on bot disconnect or call end
    
    * Improve logging

--- server/session.go
+++ server/session.go
@@ -304,6 +304,15 @@ func (p *Plugin) removeSession(us *session) error {
 	// Checking if the recording has ended due to the bot leaving.
 	if prevState.Call != nil && prevState.Call.Recording != nil && currState.Call != nil && currState.Call.Recording != nil &&
 		currState.Call.Recording.EndAt > prevState.Call.Recording.EndAt {
+
+		p.LogDebug("recording bot left the call, attempting to stop job", "channelID", us.channelID, "jobID", currState.Call.Recording.JobID)
+
+		// We still want to try to stop the recording in case the bot session disconnected without
+		// actually exiting the job.
+		if err := p.jobService.StopJob(currState.Call.Recording.JobID); err != nil {
+			p.LogError("failed to stop recording job", "error", err.Error(), "channelID", us.channelID, "jobID", currState.Call.Recording.JobID)
+		}
+
 		p.publishWebSocketEvent(wsEventCallRecordingState, map[string]interface{}{
 			"callID":   us.channelID,
 			"recState": currState.Call.Recording.getClientState().toMap(),
@@ -314,7 +323,7 @@ func (p *Plugin) removeSession(us *session) error {
 	if currState.Call != nil && currState.Call.Recording != nil && len(currState.Call.Users) == 1 && currState.Call.Users[p.getBotID()] != nil {
 		p.LogDebug("all users left call with recording in progress, stopping", "channelID", us.channelID, "jobID", currState.Call.Recording.JobID)
 		if err := p.jobService.StopJob(currState.Call.Recording.JobID); err != nil {
-			p.LogError("failed to stop recording job", "error", err.Error(), "jobID", currState.Call.Recording.JobID)
+			p.LogError("failed to stop recording job", "error", err.Error(), "channelID", us.channelID, "jobID", currState.Call.Recording.JobID)
 		}
 	}
 
