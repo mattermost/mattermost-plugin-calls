236bcafb7e0c6867b7084ee140452f0362145f64
Christopher Poile <cpoile@gmail.com>
2023-03-28 10:37:48 -0400

    MM-51589 - Fix: Global widget not showing recording alert banner (#386)
    * handle recording state in standalone code
    
    * add e2e tests

--- e2e/tests/global_widget.spec.ts
+++ e2e/tests/global_widget.spec.ts
@@ -1,4 +1,4 @@
-import {test, expect, chromium} from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
 import {userState, baseURL, defaultTeam, pluginID} from '../constants';
 
@@ -15,6 +15,46 @@ test.describe('global widget', () => {
         await page.goto(`${baseURL}/plugins/${pluginID}/standalone/widget.html?call_id=${channel.id}`);
         await expect(page.locator('#calls-widget')).toBeVisible();
         await expect(page.locator('#calls-widget-leave-button')).toBeVisible();
+        await page.locator('#calls-widget-leave-button').click();
+        await expect(page.locator('#calls-widget')).toBeHidden();
+    });
+
+    test('recording widget banner', async ({page, request, context}) => {
+        // start call
+        const channelName = getChannelNamesForTest()[0];
+        const resp = await request.get(`${baseURL}/api/v4/teams/name/${defaultTeam}/channels/name/${channelName}`);
+        const channel = await resp.json();
+
+        await page.goto(`${baseURL}/plugins/${pluginID}/standalone/widget.html?call_id=${channel.id}`);
+        await expect(page.locator('#calls-widget')).toBeVisible();
+
+        // open popout to control recording
+        const [popOut, _] = await Promise.all([
+            context.waitForEvent('page'),
+            page.click('#calls-widget-expand-button'),
+        ]);
+        await expect(popOut.locator('#calls-expanded-view')).toBeVisible();
+
+        // start recording
+        await expect(popOut.locator('#calls-popout-record-button')).toBeVisible();
+        await popOut.locator('#calls-popout-record-button').click();
+
+        // verify recording banner renders correctly
+        await expect(page.getByTestId('calls-widget-banner-recording')).toBeVisible();
+        await expect(page.getByTestId('calls-widget-banner-recording')).toContainText('You\'re recording');
+
+        // close prompt
+        await page.getByTestId('calls-widget-banner-recording').locator('.icon-close').click();
+        await expect(page.getByTestId('calls-widget-banner-recording')).toBeHidden();
+
+        // stop recording
+        await popOut.locator('#calls-popout-record-button').click();
+
+        // very recording ended prompt renders correctly
+        await expect(page.getByTestId('calls-widget-banner-recording')).toBeVisible();
+        await expect(page.getByTestId('calls-widget-banner-recording')).toContainText('Recording has stopped. Processing…');
+
+        // leave call
         await page.locator('#calls-widget-leave-button').click();
         await expect(page.locator('#calls-widget')).toBeHidden();
     });
