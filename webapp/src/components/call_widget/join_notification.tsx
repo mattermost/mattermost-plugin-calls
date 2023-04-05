edb73adc21830dba6936d25a6dc273cfbd7245dd
Claudio Costa <cstcld91@gmail.com>
2023-04-05 12:30:12 -0600

    [MM-51218] Fix global widget bounds (#389)
    * Fix global widget bounds
    
    * Fix translations
    
    * Fix snapshots

---
+++ webapp/src/components/call_widget/join_notification.tsx
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {useIntl} from 'react-intl';
+
+import MutedIcon from 'src/components/icons/muted_icon';
+import UnmutedIcon from 'src/components/icons/unmuted_icon';
+
+export type Props = {
+    visible: boolean,
+}
+
+export default function JoinNotification(props: Props) {
+    const {formatMessage} = useIntl();
+    const [animationEnded, setAnimationEnded] = useState(false);
+
+    if (!props.visible || animationEnded) {
+        return null;
+    }
+
+    const onAnimationEnd = () => {
+        setAnimationEnded(true);
+    };
+
+    const isMuted = window.callsClient?.isMuted();
+    const MuteIcon = isMuted ? MutedIcon : UnmutedIcon;
+
+    const muteIcon = (
+        <MuteIcon
+            style={{
+                width: '11px',
+                height: '11px',
+                fill: isMuted ? 'var(--center-channel-color)' : '#3DB887',
+            }}
+        />
+    );
+
+    const notificationContent = isMuted ? formatMessage({
+        defaultMessage: 'You\'re muted. Select {muteIcon} to unmute.',
+    }, {muteIcon}) : formatMessage({
+        defaultMessage: 'You\'re unmuted. Select {muteIcon} to mute.',
+    }, {muteIcon});
+
+    return (
+        <div
+            className='calls-notification-bar calls-slide-top'
+            data-testid={'calls-widget-on-join-notification'}
+            onAnimationEnd={onAnimationEnd}
+        >
+            {notificationContent}
+        </div>
+    );
+}
