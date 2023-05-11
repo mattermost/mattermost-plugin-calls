c80dd4b96ee418c71573f8b75b366bb84a8f497e
Christopher Poile <cpoile@gmail.com>
2023-05-11 05:35:28 -0400

    MM-48534 - Fix: Call post component honouring user's time format (#413)
    * move to luxon in custom_post; respect 24 hr format
    
    * tests
    
    * change 'Lasted' resolution to minutes (remove seconds)
    
    * remove moment; add 'a few seconds' to toHuman helper
    
    * generalize Timestamp; fix standalone timestamp formatting
    
    * add callStartedTimestampFn utility fn; add `a few minutes ago`
    
    * i18n

---
+++ webapp/src/components/timestamp.tsx
@@ -0,0 +1,23 @@
+import {DateTime} from 'luxon';
+import React, {useState, useEffect} from 'react';
+
+type Props = {
+    interval?: number,
+    timestampFn?: () => string,
+}
+
+function getTimestamp() {
+    return DateTime.utc().toFormat('yyyy-MM-dd HH:mm') + ' UTC';
+}
+
+export default function Timestamp({interval = 1000, timestampFn = getTimestamp}: Props) {
+    // This is needed to force a re-render to periodically update
+    // the time displayed.
+    const [, updateState] = useState({});
+    useEffect(() => {
+        const timer = setInterval(() => updateState({}), interval);
+        return () => clearInterval(timer);
+    });
+
+    return (<>{timestampFn()}</>);
+}
