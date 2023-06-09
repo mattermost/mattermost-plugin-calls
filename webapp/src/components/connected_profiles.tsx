f5900a6994e6013b3de6061b01da6b3c0f822212
Caleb Roseland <caleb@calebroseland.com>
2023-06-09 12:23:19 -0500

    MM-53004: apply intl to username conjunctions (#443)
    * use formatList
    
    * fix lint
    
    * use formatList for raised hands
    
    * keep users list bold
    
    * refactor: common-ize limit utils
    
    * dynamic max
    
    * correct max
    
    * style long to match others
    
    * move to constants, set to 8

--- webapp/src/components/connected_profiles.tsx
+++ webapp/src/components/connected_profiles.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {OverlayTrigger, Tooltip} from 'react-bootstrap';
+import {useIntl} from 'react-intl';
 
 import {UserProfile} from '@mattermost/types/users';
 
-import {getUsersList, getUserDisplayName} from '../utils';
+import {getUserDisplayName, split} from '../utils';
 
 import Avatar from './avatar/avatar';
 
@@ -17,10 +18,9 @@ interface Props {
 }
 
 const ConnectedProfiles = ({pictures, profiles, maxShowedProfiles, size, fontSize, border}: Props) => {
+    const {formatList} = useIntl();
     maxShowedProfiles = maxShowedProfiles || 2;
-    const diff = profiles.length - maxShowedProfiles;
-
-    const showedProfiles = diff > 0 ? profiles.slice(0, maxShowedProfiles) : profiles;
+    const [showedProfiles, overflowedProfiles] = split(profiles, maxShowedProfiles);
 
     const els = showedProfiles.map((profile, idx) => {
         return (
@@ -43,8 +43,7 @@ const ConnectedProfiles = ({pictures, profiles, maxShowedProfiles, size, fontSiz
         );
     });
 
-    if (diff > 0) {
-        profiles = profiles.slice(showedProfiles.length);
+    if (overflowedProfiles) {
         els.push(
             <OverlayTrigger
                 placement='bottom'
@@ -53,13 +52,13 @@ const ConnectedProfiles = ({pictures, profiles, maxShowedProfiles, size, fontSiz
                     <Tooltip
                         id='call-profiles'
                     >
-                        {getUsersList(profiles)}
+                        {formatList(overflowedProfiles.map((user) => getUserDisplayName(user)))}
                     </Tooltip>
                 }
             >
                 <Avatar
                     size={size}
-                    text={`+${diff}`}
+                    text={`+${overflowedProfiles.length}`}
                     border={Boolean(border)}
                     key='call_thread_more_profiles'
                 />
