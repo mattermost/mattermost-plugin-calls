731d6f73e2ba68160a6b3ac2919c8bef1e981ba4
Christopher Poile <cpoile@gmail.com>
2023-07-24 23:02:42 -0400

    MM-53320 - Tooltip for the incoming condensed notification message (#469)
    * add a tooltip to condensed notification when needed; tweak msg
    
    * fix tooltip name limit; use useEffect

--- webapp/src/components/incoming_calls/call_incoming_condensed.tsx
+++ webapp/src/components/incoming_calls/call_incoming_condensed.tsx
@@ -1,22 +1,22 @@
 import {GlobalState} from '@mattermost/types/store';
 import {Client4} from 'mattermost-redux/client';
-import {getTeammateNameDisplaySetting} from 'mattermost-redux/selectors/entities/preferences';
 import {getUser} from 'mattermost-redux/selectors/entities/users';
-import {displayUsername} from 'mattermost-redux/utils/user_utils';
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 import {FormattedMessage, useIntl} from 'react-intl';
 import {useSelector} from 'react-redux';
 
 import styled, {css} from 'styled-components';
 
-import {useDismissJoin, useRingingAndNotification} from 'src/components/incoming_calls/hooks';
-
 import Avatar from 'src/components/avatar/avatar';
-
 import {Button} from 'src/components/buttons';
 import CompassIcon from 'src/components/icons/compassIcon';
-
-import {IncomingCallNotification} from 'src/types/types';
+import {
+    useDismissJoin,
+    useGetCallerNameAndOthers,
+    useRingingAndNotification,
+} from 'src/components/incoming_calls/hooks';
+import {ChannelType, IncomingCallNotification} from 'src/types/types';
 
 type Props = {
     call: IncomingCallNotification;
@@ -27,22 +27,34 @@ type Props = {
 
 export const CallIncomingCondensed = ({call, onWidget = false, joinButtonBorder = false, className}: Props) => {
     const {formatMessage} = useIntl();
-    const teammateNameDisplay = useSelector(getTeammateNameDisplaySetting);
+    const messageRef = useRef<HTMLDivElement>(null);
     const caller = useSelector((state: GlobalState) => getUser(state, call.callerID));
+    const [callerName, others] = useGetCallerNameAndOthers(call, 10);
+    const [showTooltip, setShowTooltip] = useState(false);
 
     useRingingAndNotification(call, onWidget);
     const [onDismiss, onJoin] = useDismissJoin(call.channelID, call.callID);
 
-    const callerName = displayUsername(caller, teammateNameDisplay, false);
+    useEffect(() => {
+        const show = Boolean(messageRef?.current && messageRef.current.clientWidth < messageRef.current.scrollWidth);
+        setShowTooltip(show);
+    }, [messageRef]);
+
     const message = (
         <FormattedMessage
-            defaultMessage={'Call from <b>{callerName}</b>'}
+            defaultMessage={'Call from <b>{callerName}</b> with <b>{others}</b>'}
             values={{
                 b: (text: string) => <b>{text}</b>,
                 callerName,
+                others,
             }}
         />
     );
+
+    let tooltip = formatMessage({defaultMessage: 'Call from {callerName}'}, {callerName});
+    if (call.type === ChannelType.GM) {
+        tooltip = formatMessage({defaultMessage: 'Call from {callerName} with {others}'}, {callerName, others});
+    }
 
     return (
         <Container className={className}>
@@ -52,9 +64,19 @@ export const CallIncomingCondensed = ({call, onWidget = false, joinButtonBorder 
                     size={20}
                     border={false}
                 />
-                <Message>
-                    {message}
-                </Message>
+                <OverlayTrigger
+                    placement='top'
+                    overlay={
+                        <Tooltip id='tooltip-invite-message'>
+                            {tooltip}
+                        </Tooltip>
+                    }
+                    trigger={showTooltip ? ['hover', 'focus'] : []}
+                >
+                    <Message ref={messageRef}>
+                        {message}
+                    </Message>
+                </OverlayTrigger>
                 <SmallJoinButton
                     border={joinButtonBorder}
                     onClick={onJoin}
@@ -96,7 +118,7 @@ const Message = styled.div`
     margin-right: auto;
 `;
 
-const SmallJoinButton = styled(Button)<{border: boolean}>`
+const SmallJoinButton = styled(Button)<{ border: boolean }>`
     justify-content: center;
     height: 24px;
     padding: 4px 10px 4px 0;
@@ -115,7 +137,6 @@ const SmallJoinButton = styled(Button)<{border: boolean}>`
             background-color: rgba(var(--button-color-rgb), 0.12);
         }
     `}
-
     i {
         font-size: 15px;
         margin: 0 2px 0 5px;
