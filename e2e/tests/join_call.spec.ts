8616cc6ac789ee05c35f5b769c8521616f20fe51
streamer45 <cstcld91@gmail.com>
2023-11-09 15:55:56 -0600

    Bump test timeout

--- e2e/tests/join_call.spec.ts
+++ e2e/tests/join_call.spec.ts
@@ -114,7 +114,7 @@ test.describe('join call', () => {
     });
 
     test('multiple sessions per user', async ({page}) => {
-        test.setTimeout(150000);
+        test.setTimeout(180000);
 
         // start a call
         const sessionAPage = await startCall(userStorages[1]);
