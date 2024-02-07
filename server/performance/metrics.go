959e2fb23a4474b0733d3a1a94af8def354be90f
Christopher Poile <cpoile@gmail.com>
2024-02-07 16:36:32 -0500

    live captions metrics: buckets

--- server/performance/metrics.go
+++ server/performance/metrics.go
@@ -125,6 +125,7 @@ func NewMetrics() *Metrics {
 			Subsystem: metricsSubSystemLiveCaptions,
 			Name:      "new_audio_len_ms",
 			Help:      "Length (in ms) of new audio transcribed for live captions",
+			Buckets:   prometheus.LinearBuckets(1000, 250, 20),
 		},
 	)
 	m.registry.MustRegister(m.LiveCaptionsNewAudioLenHistogram)
