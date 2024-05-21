f7948789fc5608b96c962ed480d1288926d98dc8
Christopher Poile <cpoile@gmail.com>
2024-05-21 16:21:02 -0400

    new designs for join notifications

--- webapp/src/components/call_widget/component.tsx
+++ webapp/src/components/call_widget/component.tsx
@@ -10,7 +10,7 @@ import {IDMappedObjects} from '@mattermost/types/utilities';
 import {Client4} from 'mattermost-redux/client';
 import {isDirectChannel, isGroupChannel, isOpenChannel, isPrivateChannel} from 'mattermost-redux/utils/channel_utils';
 import React, {CSSProperties} from 'react';
-import {IntlShape} from 'react-intl';
+import {FormattedMessage, IntlShape} from 'react-intl';
 import {compareSemVer} from 'semver-parser';
 import {hostRemove} from 'src/actions';
 import {navigateToURL} from 'src/browser_routing';
@@ -1523,8 +1523,6 @@ export default class CallWidget extends React.PureComponent<Props, State> {
             return null;
         }
 
-        const {formatMessage} = this.props.intl;
-
         const joinedUsers = this.props.recentlyJoinedUsers.map((userID) => {
             if (userID === this.props.currentUserID) {
                 return null;
@@ -1551,7 +1549,13 @@ export default class CallWidget extends React.PureComponent<Props, State> {
                         border={false}
                     />
                     <span style={{overflow: 'hidden', textOverflow: 'ellipsis'}}>
-                        {formatMessage({defaultMessage: '{participant} has joined the call.'}, {participant: getUserDisplayName(profile)})}
+                        <FormattedMessage
+                            defaultMessage={'<b>{participant}</b> has joined the call.'}
+                            values={{
+                                b: (text: string) => <b>{text}</b>,
+                                participant: getUserDisplayName(profile),
+                            }}
+                        />
                     </span>
                 </div>
             );
