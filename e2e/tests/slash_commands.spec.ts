4935f50a2b1dc35663f97d259c56f97f9b79e34e
Christopher Poile <cpoile@gmail.com>
2024-05-22 09:50:07 -0400

    test that second host can use /call end

--- e2e/tests/slash_commands.spec.ts
+++ e2e/tests/slash_commands.spec.ts
@@ -2,7 +2,10 @@ import {chromium, expect, test} from '@playwright/test';
 import {readFile} from 'fs/promises';
 
 import PlaywrightDevPage from '../page';
-import {getUserIdxForTest, getUserStoragesForTest} from '../utils';
+import {getUserIdxForTest, getUsernamesForTest, getUserStoragesForTest, joinCall, startCall} from '../utils';
+
+const userStorages = getUserStoragesForTest();
+const usernames = getUsernamesForTest();
 
 test.beforeEach(async ({page, context}) => {
     const devPage = new PlaywrightDevPage(page);
@@ -20,11 +23,30 @@ test.describe('slash commands', () => {
         await expect(page.locator('#calls-widget')).toBeVisible();
         await expect(page.getByTestId('calls-widget-loading-overlay')).toBeHidden();
 
-        await devPage.endCall();
+        await devPage.slashCallEnd();
 
         // /call end cleans up all indicators of a running call
         await expect(page.locator('#calls-widget')).toBeHidden();
         await expect(page.locator('#calls-channel-toast')).toBeHidden();
         await expect(page.locator(`#sidebarItem_calls${userIdx}`).getByTestId('calls-sidebar-active-call-icon')).toBeHidden();
+    });
+
+    test('end call as second host', async () => {
+        const user0Page = await startCall(userStorages[0]);
+        const user1Page = await joinCall(userStorages[1]);
+
+        await expect(user0Page.page.locator('#calls-widget')).toBeVisible();
+        await expect(user0Page.page.getByTestId('calls-widget-loading-overlay')).toBeHidden();
+
+        await user0Page.sendSlashCommand(`/call host @${usernames[1]}`);
+        await user0Page.wait(1000);
+        await user0Page.expectHostToBe(usernames[1]);
+
+        await user1Page.slashCallEnd();
+
+        // /call end cleans up all indicators of a running call
+        await expect(user0Page.page.locator('#calls-widget')).toBeHidden();
+        await expect(user0Page.page.locator('#calls-channel-toast')).toBeHidden();
+        await expect(user0Page.page.locator(`#sidebarItem_calls${userIdx}`).getByTestId('calls-sidebar-active-call-icon')).toBeHidden();
     });
 });
