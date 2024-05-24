8934ce0a8da0e8dcee9bd7986ee2162e97707d89
Christopher Poile <cpoile@gmail.com>
2024-05-24 10:59:17 -0400

    keep backward compatibility for mobile

--- server/api_router.go
+++ server/api_router.go
@@ -92,6 +92,9 @@ func (p *Plugin) newAPIRouter() *mux.Router {
 	router.HandleFunc("/calls/{channel_id:[a-z0-9]{26}}/dismiss-notification", p.handleDismissNotification).Methods("POST")
 	router.HandleFunc("/calls/{call_id:[a-z0-9]{26}}/recording/{action}", p.handleRecordingAction).Methods("POST")
 
+	// Deprecated for hostCtrlRounder /end, but needed for mobile backward compatibility (pre 2.18)
+	router.HandleFunc("/calls/{call_id:[a-z0-9]{26}}/end", p.handleEnd).Methods("POST")
+
 	// Host Controls
 	hostCtrlRouter := router.PathPrefix("/calls/{call_id:[a-z0-9]{26}}/host").Subrouter()
 	hostCtrlRouter.HandleFunc("/make", p.handleMakeHost).Methods("POST")
