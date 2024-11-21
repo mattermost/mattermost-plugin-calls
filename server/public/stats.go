cb446cc860b150453b5a8a079e678ec4ddd8bd26
Ben Schumacher <ben.schumacher@mattermost.com>
2024-11-21 18:35:14 +0100

    Reuse CallsStats struct

--- server/public/stats.go
+++ server/public/stats.go
@@ -2,23 +2,23 @@ package public
 
 type CallsStats struct {
 	// The total number calls.
-	TotalCalls int64 `json:"total_calls"`
+	TotalCalls int64 `json:"total_calls" yaml:"total_calls"`
 	// The total number of active calls.
-	TotalActiveCalls int64 `json:"total_active_calls"`
+	TotalActiveCalls int64 `json:"total_active_calls" yaml:"total_active_calls"`
 	// The total number of active sessions.
-	TotalActiveSessions int64 `json:"total_active_sessions"`
+	TotalActiveSessions int64 `json:"total_active_sessions" yaml:"total_active_sessions"`
 	// The number of daily calls in the last 30 days.
-	CallsByDay map[string]int64 `json:"calls_by_day"`
+	CallsByDay map[string]int64 `json:"calls_by_day" yaml:"-"`
 	// The number of monthly calls in the last 12 months.
-	CallsByMonth map[string]int64 `json:"calls_by_month"`
+	CallsByMonth map[string]int64 `json:"calls_by_month" yaml:"-"`
 	// The distribution of calls in different channel types.
-	CallsByChannelType map[string]int64 `json:"calls_by_channel_type"`
+	CallsByChannelType map[string]int64 `json:"calls_by_channel_type" yaml:"-"`
 	// The average calls duration in seconds.
-	AvgDuration int64 `json:"avg_duration"`
+	AvgDuration int64 `json:"avg_duration" yaml:"avg_duration"`
 	// The average peak number of participants in calls.
-	AvgParticipants int64 `json:"avg_participants"`
+	AvgParticipants int64 `json:"avg_participants" yaml:"avg_participants"`
 	// The number of daily recording jobs in the last 30 days.
-	RecordingJobsByDay map[string]int64 `json:"recording_jobs_by_day"`
+	RecordingJobsByDay map[string]int64 `json:"recording_jobs_by_day" yaml:"-"`
 	// The number of monthly recording jobs in the last 12 months.
-	RecordingJobsByMonth map[string]int64 `json:"recording_jobs_by_month"`
+	RecordingJobsByMonth map[string]int64 `json:"recording_jobs_by_month" yaml:"-"`
 }
--- server/support_packet.go
+++ server/support_packet.go
@@ -9,22 +9,15 @@ import (
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
 
+	"github.com/mattermost/mattermost-plugin-calls/server/public"
 	"github.com/mattermost/mattermost/server/public/model"
 	"github.com/mattermost/mattermost/server/public/plugin"
 )
 
 type SupportPacket struct {
 	Version string `yaml:"version"`
-	// The total number calls.
-	TotalCalls int64 `yaml:"total_calls"`
-	// The total number of active calls.
-	TotalActiveCalls int64 `yaml:"total_active_calls"`
-	// The total number of active sessions.
-	TotalActiveSessions int64 `yaml:"total_active_sessions"`
-	// The average calls duration in seconds.
-	AvgDuration int64 `yaml:"avg_duration"`
-	// The average peak number of participants in calls.
-	AvgParticipants int64 `yaml:"avg_participants"`
+
+	*public.CallsStats
 }
 
 func (p *Plugin) GenerateSupportData(_ *plugin.Context) ([]*model.FileData, error) {
@@ -34,12 +27,8 @@ func (p *Plugin) GenerateSupportData(_ *plugin.Context) ([]*model.FileData, erro
 	}
 
 	diagnostics := SupportPacket{
-		Version:             manifest.Version,
-		TotalCalls:          stats.TotalCalls,
-		TotalActiveCalls:    stats.TotalCalls,
-		TotalActiveSessions: stats.TotalActiveSessions,
-		AvgDuration:         stats.AvgDuration,
-		AvgParticipants:     stats.AvgParticipants,
+		Version:    manifest.Version,
+		CallsStats: stats,
 	}
 	b, err := yaml.Marshal(diagnostics)
 	if err != nil {
