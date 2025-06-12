c85429a1b924afc819570a0c1d69bf32f46513c6
Claudio Costa <cstcld91@gmail.com>
2025-06-12 09:51:49 -0600

    [MM-64539] Implement simple ping mechanism in WebSocket client (#1043)
    * Implement simple ping mechanism in WebSocket client
    
    * test: add tests for pendingPingSeq in websocket client
    
    * Address feedback
    
    * Rename for clarity

--- webapp/src/websocket.ts
+++ webapp/src/websocket.ts
@@ -10,6 +10,7 @@ import {pluginId} from './manifest';
 const wsMinReconnectRetryTimeMs = 1000; // 1 second
 const wsReconnectionTimeout = 30000; // 30 seconds
 const wsReconnectTimeIncrement = 500; // 0.5 seconds
+const wsPingIntervalMs = 5000; // 5 seconds
 
 export enum WebSocketErrorType {
     Native,
@@ -42,6 +43,9 @@ export class WebSocketClient extends EventEmitter {
     private lastDisconnect = 0;
     private reconnectRetryTime = wsMinReconnectRetryTimeMs;
     private closed = false;
+    private pingInterval: ReturnType<typeof setInterval> | null = null;
+    private waitingForPong = false;
+    private expectedPongSeqNo = 0;
 
     constructor(wsURL: string, authToken?: string) {
         super();
@@ -67,23 +71,24 @@ export class WebSocketClient extends EventEmitter {
                 }));
             }
             if (isReconnect) {
-                logDebug('ws: reconnected');
+                logDebug('ws: reconnected', this.originalConnID, this.connID);
                 this.lastDisconnect = 0;
                 this.reconnectRetryTime = wsMinReconnectRetryTimeMs;
                 this.emit('open', this.originalConnID, this.connID, true);
             }
+
+            // Start ping interval
+            this.startPingInterval();
+
+            // Send initial ping.
+            this.ping();
         };
 
         this.ws.onerror = () => {
             this.emit('error', new WebSocketError(WebSocketErrorType.Native, 'websocket error'));
         };
 
-        this.ws.onclose = ({code}) => {
-            this.emit('close', code);
-            if (!this.closed) {
-                this.reconnect();
-            }
-        };
+        this.ws.onclose = this.closeHandler;
 
         this.ws.onmessage = ({data}) => {
             if (!data) {
@@ -97,6 +102,13 @@ export class WebSocketClient extends EventEmitter {
                 return;
             }
 
+            // Handle pong response
+            if (this.waitingForPong && msg?.seq_reply === this.expectedPongSeqNo) {
+                this.waitingForPong = false;
+                this.expectedPongSeqNo = 0;
+                return;
+            }
+
             if (msg) {
                 this.serverSeqNo = msg.seq + 1;
             }
@@ -107,11 +119,12 @@ export class WebSocketClient extends EventEmitter {
 
             if (msg.event === 'hello') {
                 if (msg.data.connection_id !== this.connID) {
-                    logDebug('ws: new conn id from server');
+                    logDebug('ws: new conn id from server', msg.data.connection_id);
                     this.connID = msg.data.connection_id;
                     this.serverSeqNo = 0;
+                    this.seqNo = 1;
                     if (this.originalConnID === '') {
-                        logDebug('ws: setting original conn id');
+                        logDebug('ws: setting original conn id', this.connID);
                         this.originalConnID = this.connID;
                     }
 
@@ -146,12 +159,21 @@ export class WebSocketClient extends EventEmitter {
         };
     }
 
+    private closeHandler = (ev: CloseEvent) => {
+        this.stopPingInterval();
+        this.emit('close', ev.code);
+        if (!this.closed) {
+            this.reconnect();
+        }
+    };
+
     send(action: string, data?: Record<string, unknown>, binary?: boolean) {
         const msg = {
             action: `${this.eventPrefix}_${action}`,
             seq: this.seqNo++,
             data,
         };
+
         if (this.ws && this.ws.readyState === WebSocket.OPEN) {
             if (binary) {
                 this.ws.send(encode(msg));
@@ -165,10 +187,12 @@ export class WebSocketClient extends EventEmitter {
 
     close() {
         this.closed = true;
+        this.stopPingInterval();
         this.ws?.close();
         this.ws = null;
         this.seqNo = 1;
         this.serverSeqNo = 0;
+        this.expectedPongSeqNo = 0;
         this.connID = '';
         this.originalConnID = '';
 
@@ -194,7 +218,7 @@ export class WebSocketClient extends EventEmitter {
 
         setTimeout(() => {
             if (!this.closed) {
-                logInfo('ws: reconnecting');
+                logInfo('ws: reconnecting', this.originalConnID);
                 this.init(true);
             }
         }, this.reconnectRetryTime);
@@ -204,5 +228,54 @@ export class WebSocketClient extends EventEmitter {
 
     getOriginalConnID() {
         return this.originalConnID;
+    }
+
+    private startPingInterval() {
+        if (this.pingInterval) {
+            this.stopPingInterval();
+        }
+
+        logDebug('ws: starting ping interval', this.originalConnID);
+
+        this.pingInterval = setInterval(() => {
+            if (this.waitingForPong && this.ws) {
+                logWarn('ws: ping timeout, reconnecting', this.originalConnID);
+
+                // We call the close handler directly since through ws.close() it could execute after a significant delay.
+                this.ws.onclose = null;
+                this.ws.close();
+                this.closeHandler(new CloseEvent('close', {
+                    code: 4000,
+                }));
+
+                return;
+            }
+
+            this.ping();
+        }, wsPingIntervalMs);
+    }
+
+    private stopPingInterval() {
+        if (this.pingInterval) {
+            logDebug('ws: stopping ping interval', this.originalConnID);
+            clearInterval(this.pingInterval);
+            this.pingInterval = null;
+            this.waitingForPong = false;
+            this.expectedPongSeqNo = 0;
+        }
+    }
+
+    private ping() {
+        if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+            this.waitingForPong = true;
+
+            // This is used to track the expected pong response which should match the request's sequence number.
+            this.expectedPongSeqNo = this.seqNo;
+
+            this.ws.send(JSON.stringify({
+                action: 'ping',
+                seq: this.seqNo++,
+            }));
+        }
     }
 }
